a37e3215c272585b8d6313c5a646f6c48e98ac37
Guilherme Gianjope <guigianjope@gmail.com>
2022-02-11 16:35:51 -0800

    logo and menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,21 @@ import './Navbar.css';
 
 const Navbar = () => (
   <nav className='app__navbar'>
-    Navbar
+    <div className='app__navbar-logo'>
+      GIANJOPE
+    </div>
+    <ul className='app__navbar-links'>
+      <li className='p__opensans'><a href='#home'>Home</a></li>
+      <li className='p__opensans'><a href='#about'>About</a></li>
+      <li className='p__opensans'><a href='#menu'>Menu</a></li>
+      <li className='p__opensans'><a href='#awards'>Awards</a></li>
+      <li className='p__opensans'><a href='#contact'>Contact</a></li>
+    </ul>
+    <div className='app__navbar-login'>
+      <a href='#login' className='p__opensans' >Log In / Register</a>
+      <div/>
+      <a href='/' className='p__opensans'>Book Table</a>
+    </div>
   </nav>
 );
 
