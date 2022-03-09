e636692d403086b23ca4f707e56f0e9f35f44846
Guilherme Gianjope <guigianjope@gmail.com>
2022-03-09 14:34:13 -0800

    responsive menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { GiHamburguer } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
-
-import images from '../../constants/images';
+import imagSes from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
@@ -21,6 +20,24 @@ const Navbar = () => (
       <a href='#login' className='p__opensans' >Log In / Register</a>
       <div/>
       <a href='/' className='p__opensans'>Book Table</a>
+    </div>
+    <div className='app__navbar-smallscreen'>
+      <GiHamburgerMenu color="#fff" fontSize={2} onClick={() => {}}/>
+        <div className='app__navbar-smallscren_overlay flex__center slide-bottom'>
+          <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}}/>
+            <ul className='app__navbar-smallscren-links'>
+              <li className='p__opensans'><a href='#home'>Home</a></li>
+              <li className='p__opensans'><a href='#about'>About</a></li>
+              <li className='p__opensans'><a href='#menu'>Menu</a></li>
+              <li className='p__opensans'><a href='#awards'>Awards</a></li>
+              <li className='p__opensans'><a href='#contact'>Contact</a></li>
+            </ul>
+        </div>
+
+        
+
+
+        
     </div>
   </nav>
 );
